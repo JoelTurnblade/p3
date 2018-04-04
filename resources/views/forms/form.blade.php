feb87468bed65aa0f7798b6bf24f1b083cb84806
Joel Turnblade <joel.turnblade@gmail.com>
2018-04-04 15:43:30 -0700

    Replaced null with empty string in hopes of solving W3 validator error

--- resources/views/forms/form.blade.php
+++ resources/views/forms/form.blade.php
@@ -9,27 +9,27 @@
             <option value='Multiply' @if(old('operation', '') == 'Multiply') {{ 'selected' }} @endif>Multiply</option>
             <option value='Divide' @if(old('operation', '') == 'Divide') {{ 'selected' }} @endif>Int Divide</option>
         </select>
-        <input type='hidden' name='preOperation' value='{{ $operation or old('operation', null) }}'>
+        <input type='hidden' name='preOperation' value='{{ $operation or old('operation', '') }}'>
         <br>
         Digits in first number
         <input type='radio' name='firstNumDig' value='1' @if(in_array(old('firstNumDig'), ['1', ''])) {{ 'checked' }} @endif>1
         <input type='radio' name='firstNumDig' value='2' @if(old('firstNumDig', '') == '2') {{ 'checked' }} @endif>2
         <input type='radio' name='firstNumDig' value='3' @if(old('firstNumDig', '') == '3') {{ 'checked' }} @endif>3
         <input type='radio' name='firstNumDig' value='4' @if(old('firstNumDig', '') == '4') {{ 'checked' }} @endif>4
-        <input type='hidden' name='preFirstNumDig' value='{{ $firstNumDig or old('operation', null) }}'>
+        <input type='hidden' name='preFirstNumDig' value='{{ $firstNumDig or old('operation', '') }}'>
         <br>
         Digits in second number
         <input type='radio' name='secondNumDig' value='1' @if(in_array(old('secondNumDig'), ['1', ''])) {{ 'checked' }} @endif>1
         <input type='radio' name='secondNumDig' value='2' @if(old('secondNumDig', '') == '2') {{ 'checked' }} @endif>2
         <input type='radio' name='secondNumDig' value='3' @if(old('secondNumDig', '') == '3') {{ 'checked' }} @endif>3
         <input type='radio' name='secondNumDig' value='4' @if(old('secondNumDig', '') == '4') {{ 'checked' }} @endif>4
-        <input type='hidden' name='preSecondNumDig' value='{{ $secondNumDig or old('operation', null) }}'>
+        <input type='hidden' name='preSecondNumDig' value='{{ $secondNumDig or old('operation', '') }}'>
         <br>
         <p>{!! $displayString or old('displayString', '<br>') !!}</p>
-        <input type='hidden' name='displayString' value='{{ $displayString or old('displayString', null) }}'>
-        <input type='hidden' name='firstNum' value='{{ $firstNum or old('firstNum', null) }}'>
-        <input type='hidden' name='secondNum' value='{{ $secondNum or old('secondNum', null) }}'>
-        <input type='hidden' name='correctAnswer' value='{{ $correctAnswer or old('correctAnswer', null) }}'>
+        <input type='hidden' name='displayString' value='{{ $displayString or old('displayString', '') }}'>
+        <input type='hidden' name='firstNum' value='{{ $firstNum or old('firstNum', '') }}'>
+        <input type='hidden' name='secondNum' value='{{ $secondNum or old('secondNum', '') }}'>
+        <input type='hidden' name='correctAnswer' value='{{ $correctAnswer or old('correctAnswer', '') }}'>
         <input type='text' name='userAnswer' value=@if($errors->any()) '{{ old('userAnswer', '') }}' @endif>
         <br>
         <input type='submit' value='check answer / generate'>
